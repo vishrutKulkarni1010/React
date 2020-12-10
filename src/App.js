40343bb0a7da2b4c2620cc409a5ce056aff2c177
vishrutKulkarni1010 <vishrutkulkarni1002@gmail.com>
2020-12-10 09:14:12 +0530

    Sidebar drawer

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
+import Sidebar from './Components/Sidebar';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar/>
       <h1>Hi Friend!:)</h1>
     </div>
   );
